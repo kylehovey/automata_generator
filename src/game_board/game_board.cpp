32b823fbf91099cf3ce1ea2e0846026b1f7ef876
khoveyc@gmail.com <khoveyc@gmail.com>
2020-04-16 23:47:00 -0600

    Added utility methods for changing the board

--- src/game_board/game_board.cpp
+++ src/game_board/game_board.cpp
@@ -9,22 +9,35 @@ namespace GameBoard {
     const int& height
   ) : board(Board(height, std::vector<Cell>(width))) { }
 
-  void GameBoard::advance_time() {
-    for (auto& row : this->board) {
-      for (auto& cell : row) {
-        cell.current = cell.next;
+  void GameBoard::mutate(const LocMutation& mutation) {
+    for (unsigned int y = 0; y < this->board.size(); ++y) {
+      auto row = this->board[y];
+      for (unsigned int x = 0; x < row.size(); ++x) {
+        mutation(x, y, row[x]);
       }
     }
+  }
+
+  void GameBoard::mutate(const Mutation& mutation) {
+    for (auto& row : this->board) {
+      for (auto& cell : row) {
+        mutation(cell);
+      }
+    }
+  }
+
+  void GameBoard::advance_time() {
+    this->mutate([](Cell& cell) {
+      cell.current = cell.next;
+    });
   }
 
   void GameBoard::randomize(const int& seed = 0) {
     srand(seed);
 
-    for (auto& row : this->board) {
-      for (auto& cell : row) {
-        cell.next = rand() % 2;
-      }
-    }
+    this->mutate([](Cell& cell) {
+      cell.next = rand() % 2;
+    });
 
     this->advance_time();
   }
--- src/game_board/game_board.h
+++ src/game_board/game_board.h
@@ -2,15 +2,18 @@
 #define GAME_BOARD_H
 
 #include <vector>
+#include <functional>
 
 namespace GameBoard {
   struct Cell {
-    Cell() : current(true), next(false) { };
+    Cell() : current(false), next(false) { };
     bool current;
     bool next;
   };
 
   using Board = std::vector< std::vector<Cell> >;
+  using LocMutation = std::function<void(const int&, const int&, Cell&)>;
+  using Mutation = std::function<void(Cell&)>;
 
   class GameBoard {
     public:
@@ -21,6 +24,8 @@ namespace GameBoard {
       void print() const;
     private:
       void advance_time();
+      void mutate(const LocMutation&);
+      void mutate(const Mutation&);
       Board board;
   };
 }
