96c71fb1f14e5f6e1b92752d6eac79ea2703fa61
khoveyc@gmail.com <khoveyc@gmail.com>
2020-04-16 22:07:41 -0600

    Updated example code to use lambda

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,13 @@ int main() {
   const std::string save_file = "tmp/output.pbm";
   const int width = 120;
   const int height = 64;
+  ImageWriter::PixelMap mapper = [](const int& x, const int& y) {
+    return (x + y) % 2;
+  };
 
   ImageWriter::Descriptor descriptor(save_file, width, height);
 
-  descriptor.write();
+  descriptor.write(mapper);
   descriptor.close();
 
   std::cout << "File Written." << std::endl;
