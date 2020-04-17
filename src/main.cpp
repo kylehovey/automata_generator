85d8cd59d38eac0fc8ac2d5c4ca1bee8d1b82b24
khoveyc@gmail.com <khoveyc@gmail.com>
2020-04-16 22:52:10 -0600

    Switched to game board for image output

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
 
 #include "image_writer/image_writer.cpp"
+#include "game_board/game_board.cpp"
 
 int main() {
   const std::string save_file = "tmp/output.pbm";
   const int width = 120;
   const int height = 64;
-  ImageWriter::PixelMap mapper = [](const int& x, const int& y) {
-    return (x + y) % 2;
+  const GameBoard::GameBoard board(width, height);
+
+  ImageWriter::PixelMap mapper = [&](const int& x, const int& y) {
+    return board.stateOf(x, y);
   };
 
   ImageWriter::Descriptor descriptor(save_file, width, height);
