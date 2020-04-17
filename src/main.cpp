3af711b803623746656931c4deef5a5701add025
khoveyc@gmail.com <khoveyc@gmail.com>
2020-04-16 23:24:13 -0600

    Using initializer methods in main code

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,40 @@
 #include <iostream>
 #include <vector>
-#include <stdlib.h>
 
 #include "image_writer/image_writer.cpp"
 #include "game_board/game_board.cpp"
 
 int main() {
+  /**
+   * Settings
+   */
   const std::string save_file = "tmp/output.pbm";
   const int width = 120;
   const int height = 64;
-  const GameBoard::GameBoard board(width, height);
 
+  /**
+   * Game board initialization
+   */
+  GameBoard::GameBoard board(width, height);
+  board.randomize();
+
+  /**
+   * Image data creation
+   */
   ImageWriter::PixelMap mapper = [&](const int& x, const int& y) {
     return board.stateOf(x, y);
   };
 
+  /**
+   * Write the file to output
+   */
   ImageWriter::Descriptor descriptor(save_file, width, height);
-
   descriptor.write(mapper);
   descriptor.close();
 
+  /**
+   * Let the humans know this is done
+   */
   std::cout << "File Written." << std::endl;
 
   return EXIT_SUCCESS;
