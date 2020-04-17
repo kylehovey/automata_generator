b91503a1e5bd37f2c1e393c6c7f4554248cb7703
khoveyc@gmail.com <khoveyc@gmail.com>
2020-04-16 23:23:59 -0600

    Added initializer methods to GameBoard

--- src/game_board/game_board.cpp
+++ src/game_board/game_board.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 
 #include "game_board.h"
 
@@ -8,13 +9,33 @@ namespace GameBoard {
     const int& height
   ) : board(Board(height, std::vector<Cell>(width))) { }
 
+  void GameBoard::advance_time() {
+    for (auto& row : this->board) {
+      for (auto& cell : row) {
+        cell.current = cell.next;
+      }
+    }
+  }
+
+  void GameBoard::randomize(const int& seed = 0) {
+    srand(seed);
+
+    for (auto& row : this->board) {
+      for (auto& cell : row) {
+        cell.next = rand() % 2;
+      }
+    }
+
+    this->advance_time();
+  }
+
   bool GameBoard::stateOf(const int& x, const int& y) const {
     return this->board[y][x].current;
   }
 
   void GameBoard::print() const {
-    for (const auto row : this->board) {
-      for (const Cell cell : row) {
+    for (const auto& row : this->board) {
+      for (const auto& cell : row) {
         std::cout << cell.current;
       }
 
--- src/game_board/game_board.h
+++ src/game_board/game_board.h
@@ -5,19 +5,22 @@
 
 namespace GameBoard {
   struct Cell {
+    Cell() : current(true), next(false) { };
     bool current;
     bool next;
   };
 
-  using Board = std::vector<std::vector<Cell> >;
+  using Board = std::vector< std::vector<Cell> >;
 
   class GameBoard {
     public:
       GameBoard(const int&, const int&);
 
+      void randomize(const int&);
       bool stateOf(const int&, const int&) const;
       void print() const;
     private:
+      void advance_time();
       Board board;
   };
 }
