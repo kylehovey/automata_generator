b11e400fdc81b9312c807222d9ee8cb0256512f5
khoveyc@gmail.com <khoveyc@gmail.com>
2020-04-18 12:05:43 -0600

    Updated main code to run Game of Life, added some other rules

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <time.h>
 
 #include "image_writer/image_writer.cpp"
 #include "game_board/game_board.cpp"
@@ -9,33 +10,55 @@ int main() {
    * Settings
    */
   const std::string save_file = "tmp/output.pbm";
-  const int width = 120;
-  const int height = 64;
+  const int width = 500;
+  const int height = 500;
+  const GameBoard::Rule anneal = [](const int& neighbors, const bool& alive) {
+    const bool sixseveneight = neighbors >= 6 && neighbors <= 8;
+    if (neighbors == 4 || sixseveneight) return true;
+    if (alive && (neighbors == 3 || neighbors == 5 || sixseveneight)) return true;
+    return false;
+  };
+  const GameBoard::Rule GoL = [](const int& neighbors, const bool& alive) {
+    if (neighbors == 3) return true;
+    if (alive && (neighbors == 2 || neighbors == 3)) return true;
+    return false;
+  };
+  const GameBoard::Rule seeds = [](const int& neighbors, const bool& alive) {
+    (void) alive;
+    if (neighbors == 2) return true;
+    return false;
+  };
+  const GameBoard::Rule dayAndNight = [](const int& neighbors, const bool& alive) {
+    const bool sixseveneight = neighbors >= 6 && neighbors <= 8;
+    if (neighbors == 3 || sixseveneight) return true;
+    if (alive && (neighbors == 3 || neighbors == 4 || sixseveneight)) return true;
+    return false;
+  };
 
   /**
    * Game board initialization
    */
   GameBoard::GameBoard board(width, height);
-  board.randomize();
+  board.randomize(time(NULL));
 
   /**
    * Image data creation
    */
-  ImageWriter::PixelMap mapper = [=](const int& x, const int& y) {
-    return board.state_of(x, y);
+  ImageWriter::PixelMap mapper = [&](const int& x, const int& y) {
+    return board.state_of(y, x);
   };
 
   /**
    * Write the file to output
    */
-  ImageWriter::Descriptor descriptor(save_file, width, height);
-  descriptor.write(mapper);
-  descriptor.close();
-
-  /**
-   * Let the humans know this is done
-   */
-  std::cout << "File Written." << std::endl;
+  for (int i = 0; i < 255; ++i) {
+    std::cout << "Running iteration " << i << std::endl;
+    const std::string file_name = "tmp/pbm/" + std::to_string(i) + ".pbm";
+    ImageWriter::Descriptor descriptor(file_name, width, height);
+    descriptor.write(mapper);
+    descriptor.close();
+    board.step_with_rule(GoL);
+  }
 
   return EXIT_SUCCESS;
 }
